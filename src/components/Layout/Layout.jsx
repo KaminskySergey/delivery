5065989024b12a685fdb74ae898e13efee4ec9bd
KaminskySergey <kaminsky_s_s@ukr.net>
2023-06-04 17:33:28 +0300

    style basic

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@
 import { Suspense } from "react"
 import Header from "../../pages/Header/Header"
 import { Outlet } from "react-router-dom"
+import Box from "../Box/Box"
 
 
 const Layout = ({children}) => {
@@ -10,9 +11,13 @@ const Layout = ({children}) => {
         <Header />
         
         <main>
+            <Box style={{width: '99%', borderRadius: '32px', display: 'flex'}}>
+
+            
             <Suspense>
                 <Outlet fallback={null}></Outlet>
             </Suspense>
+            </Box>
         </main>
         </>
     )
